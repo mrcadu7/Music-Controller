4b6571400e50c15f301ba44c737325b3b93425cd
Cadú <139708523+mrcadu7@users.noreply.github.com>
2024-03-21 19:27:48 -0300

    WIP: acrescentado os componentes e rotas principais

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component {
     constructor(props) {
@@ -8,7 +10,9 @@ export default class App extends Component {
 
     render() {
         return (
-            <h1>Testing react code</h1>
+            <div>
+                <HomePage />
+            </div>
         );
     }
 }
